228fd293d12baddfe5fd86a0e4f46f63e865531b
AnastasiaPonti <116596976+AnastasiaPonti@users.noreply.github.com>
2022-10-31 00:14:52 +0100

    Create laprovadelnove.java

--- laprovadelnove.java
+++ laprovadelnove.java
@@ -1,11 +1,28 @@
 package laprovadelnove;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
+
+//Scrivete un programma che, dato un numero naturale N nel flusso di ingresso, emetta tutte e sole le possibili terne
+//di interi positivi A, B e C ciascuno minore di N per cui A x B è diverso da C, ma la prova del nove funziona.
 
 public class Laprovadelnove {
   public static void main() {
-    int N;
-    System.in.scanf(N);
+    Scanner N = new Scanner(System.in);
+    System.out.println("Inserisci il numero: ");
+    int number = N.nextInt();
+    N.close();
+    System.out.println("");
+    ProvaNove(Integer.toString(number));
   }
-}
+    public static String ProvaNove(String n) {
+      int somma = 0;
+      for (int i=0; i<n.length(); i++) {
+        somma += Integer.parseInt(String.valueOf(n.charAt(i)));
+      }
+      if (somma > 9) {
+        return ProvaNove(Integer.toString(somma));
+      }
+      return Integer.toString(somma);
+    }
+  }
