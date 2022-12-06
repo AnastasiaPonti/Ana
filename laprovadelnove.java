df6703315b7e7b9524d8dab58f9ca26cf5ce0ddb
Anastasia Ponti <116596976+AnastasiaPonti@users.noreply.github.com>
2022-12-06 14:23:21 +0100

    Update laprovadelnove.java

--- laprovadelnove.java
+++ laprovadelnove.java
@@ -13,15 +13,31 @@ public class Laprovadelnove {
     int number = N.nextInt();
     N.close();
     System.out.println("");
-    ProvaNove(Integer.toString(number));
+    int a, b, c, ris;
+    for (int i=1; i<number; i++) {
+      for (int j=1; j<number; j++) {
+        for (int k=1; k<number; k++) {
+          a = Integer.parseInt(SommaCifre(Integer.parseInt(i)));
+          b = Integer.parseInt(SommaCifre(Integer.parseInt(j)));
+          c = Integer.parseInt(SommaCifre(Integer.parseInt(k)));
+          ris = a*b;
+          ris = Integer.parseInt(SommaCifre(Integer.parseInt(ris)));
+          if (ris == c) {
+            if (i*j != k) {
+              System.out.println(i, j, k + "\n");
+            }
+          }
+        }
+      }
+    }
   }
-    public static String ProvaNove(String n) {
+    public static String SommaCifre(String n) {
       int somma = 0;
       for (int i=0; i<n.length(); i++) {
         somma += Integer.parseInt(String.valueOf(n.charAt(i)));
       }
       if (somma > 9) {
-        return ProvaNove(Integer.toString(somma));
+        return SommaCifre(Integer.toString(somma));
       }
       return Integer.toString(somma);
     }
